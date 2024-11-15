62510835b100723ffafc6da24ece4dcedc2e325c
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-15 21:28:45 +0600

    done 15 min of part-1

---
+++ dist/basic.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("hello");
+console.log("hi");
+const age = 35;
+const myName = "shahid";
+const isBoolean = true;
+//Array:array is the collection of same type data
+const numbers = [1, 2, 3, 5, 6, 8];
+console.log(numbers);
+const info = ["sumi", "sohel", 35, 33]; //javascript
+console.log(info);
---
+++ src/basic.ts
@@ -0,0 +1,19 @@
+console.log("hello");
+console.log("hi");
+
+const age:number = 35;
+const myName : string = "shahid";
+const isBoolean : boolean = true;
+
+//Array:array is the collection of same type data
+const numbers :number[] =[1,2,3,5,6,8];
+console.log(numbers)
+
+const info = ["sumi","sohel",35,33,true,null,undefined]  //in javascript-->array
+const info2:[string,string,number,number,boolean,null,undefined] = ["sumi","sohel",35,33,true,null,undefined]  //in TS--->tuple
+console.log(info)
+console.log(info2)
+
+//in array store only same type data but in tuple store can different type data
+
+//15 min done
